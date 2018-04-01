fb8b01a79416a55ec8b103cff6d8226aeeb79b7b
willybeans <willwedmedyk@gmail.com>
2018-04-01 01:59:08 -0400

    adding + - functionality for buttons

--- pomodoro.js
+++ pomodoro.js
@@ -6,7 +6,44 @@ var clockTime = document.querySelector('.clock_time');
 
 window.onload = () => {
   clock.textContent = a;
-  clock.addEventListener("click", countDown);
+  //clock.addEventListener("click", countDown);
+  document.querySelector('.container').addEventListener('click', function(e){
+    //console.log(e.target);
+    if (e.target !== e.currentTarget){
+      console.log(e.target.id);
+      var clickedItem = e.target.className;
+      var itemParent = e.target.parentNode.className;
+    //  console.log(e.target.childNodes);
+      if (clickedItem === 'clock'){
+        countDown();
+      } else {
+        changeLength(clickedItem, itemParent);
+      }
+    }
+    e.stopPropagation();
+  }, false );
+}
+
+const changeLength = function(clickedItem, itemParent){
+  var numberArea = document.querySelector('.' + itemParent).childNodes[1];
+  console.log(clickedItem);
+  console.log(itemParent);
+//  var test = document.querySelector('.' + itemParent).childNodes[1].className;
+if(clickedItem === "minus"){
+  numberArea.textContent = numberArea.textContent - 1;
+} else {
+numberArea.textContent = Number(numberArea.textContent) + 1;
+}
+
+if(itemParent === "clock_time"){
+  clock.textContent = numberArea.textContent;
+}
+  //document.querySelector('.' + itemParent).childNodes[1].textContent = "hi";
+/* i think we need to get a function to split the id apart
+  and then we can do for example -> first find minus, then take the second
+  half of the id that will specify which div to subtract from?
+*/
+
 }
 
 const countDown = function(){
